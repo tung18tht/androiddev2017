53ae4933345e766e3c5c3e1ad6c9423677e66d8d
Tung Nguyen <tung18tht@gmail.com>
2016-11-19 21:15:03 +0700

    Override MainActivity lifecycle methods

--- MusicPlayer/app/src/main/java/vn/edu/usth/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/vn/edu/usth/musicplayer/MainActivity.java
@@ -1,7 +1,8 @@
 package vn.edu.usth.musicplayer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     
@@ -9,5 +10,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Log.i("status", "Created");
+    }
+    
+    @Override
+    protected void onStart() {
+        super.onStart();
+        
+        Log.i("status", "Started");
+    }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+        
+        Log.i("status", "Paused");
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        
+        Log.i("status", "Resumed");
+    }
+    
+    @Override
+    protected void onStop() {
+        super.onStop();
+        
+        Log.i("status", "Stopped");
+    }
+    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        
+        Log.i("status", "Destroyed");
     }
 }
