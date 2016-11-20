5b0dd9cfe10c2273ebe94c673993d471b7db3b2f
Tung Nguyen <tung18tht@gmail.com>
2016-11-20 23:14:13 +0700

    Initiate basic layout with bottom bar

--- MusicPlayer/app/src/main/java/vn/edu/usth/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/vn/edu/usth/musicplayer/MainActivity.java
@@ -1,8 +1,11 @@
 package vn.edu.usth.musicplayer;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class MainActivity extends AppCompatActivity {
     
@@ -10,42 +13,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                switch (tabId) {
+                    case R.id.tab_home:
+                        
+                    case R.id.tab_playing:
+                        
+                    case R.id.tab_download:
+                }
+            }
+        });
         
-        Log.i("status", "Created");
+        Log.i("status", "Main Activity created");
     }
     
     @Override
     protected void onStart() {
         super.onStart();
         
-        Log.i("status", "Started");
+        Log.i("status", "Main Activity started");
     }
     
     @Override
     protected void onPause() {
         super.onPause();
         
-        Log.i("status", "Paused");
+        Log.i("status", "Main Activity paused");
     }
     
     @Override
     protected void onResume() {
         super.onResume();
         
-        Log.i("status", "Resumed");
+        Log.i("status", "Main Activity resumed");
     }
     
     @Override
     protected void onStop() {
         super.onStop();
         
-        Log.i("status", "Stopped");
+        Log.i("status", "Main Activity stopped");
     }
     
     @Override
     protected void onDestroy() {
         super.onDestroy();
         
-        Log.i("status", "Destroyed");
+        Log.i("status", "Main Activity destroyed");
     }
 }
