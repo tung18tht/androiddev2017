e7d5d42ce746eb90bfbf9941bf40edb83ef76185
ngockha96 <ngockha96@gmail.com>
2016-11-21 22:53:36 +0700

    Add scrollview and items to home page

--- MusicPlayer/app/src/main/java/vn/edu/usth/musicplayer/fragment/HomeFragment.java
+++ MusicPlayer/app/src/main/java/vn/edu/usth/musicplayer/fragment/HomeFragment.java
@@ -6,16 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import vn.edu.usth.musicplayer.R;
 
 public class HomeFragment extends Fragment {
     public HomeFragment() {
         super();
     }
-    
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
 }
