f1592ffb265732dc2d2f9c62440d269c5fa96816
Tung Nguyen <tung18tht@gmail.com>
2016-12-05 01:51:54 +0700

    Add highlight color when hold

--- MusicPlayer/app/src/main/java/vn/edu/usth/musicplayer/fragment/SongsFragment.java
+++ MusicPlayer/app/src/main/java/vn/edu/usth/musicplayer/fragment/SongsFragment.java
@@ -6,9 +6,8 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.util.Log;
+import android.view.*;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -22,8 +21,6 @@ import vn.edu.usth.musicplayer.Model.SongAPI;
 import vn.edu.usth.musicplayer.R;
 
 import java.util.ArrayList;
-
-import static vn.edu.usth.musicplayer.fragment.SongsFragment.queue;
 
 public class SongsFragment extends Fragment {
     public static RecyclerView.Adapter songsFragmentAdapter;
@@ -58,64 +55,86 @@ public class SongsFragment extends Fragment {
         songsFragmentAdapter = new Adapter(SongAPI.getSongs(getActivity()));
         recyclerView.setAdapter(songsFragmentAdapter);
     }
-}
 
-class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
-    private ArrayList<JSONObject> data;
+    class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+        private ArrayList<JSONObject> data;
 
-    static class ViewHolder extends RecyclerView.ViewHolder {
-        RelativeLayout songView;
-        ViewHolder(RelativeLayout v) {
-            super(v);
-            songView = v;
+        class ViewHolder extends RecyclerView.ViewHolder {
+            RelativeLayout songView;
+            ViewHolder(RelativeLayout v) {
+                super(v);
+                songView = v;
+            }
+        }
+
+        Adapter(ArrayList<JSONObject> data) {
+            this.data = data;
+        }
+
+        @Override
+        public Adapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            RelativeLayout v = (RelativeLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.view_song, parent, false);
+            return new ViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            final TextView songTitle = (TextView) holder.songView.findViewById(R.id.songTitle);
+            TextView songArtist = (TextView) holder.songView.findViewById(R.id.songArtist);
+            TextView songDuration = (TextView) holder.songView.findViewById(R.id.songDuration);
+
+            JSONObject songInfoJSON = data.get(position);
+            Object songInfo = Configuration.defaultConfiguration().jsonProvider().parse(songInfoJSON.toString());
+
+            songTitle.setText((String) JsonPath.read(songInfo, "$.title"));
+            songArtist.setText((String) JsonPath.read(songInfo, "$.artist"));
+            songDuration.setText(formatTime((Integer)JsonPath.read(songInfo, "$.duration")));
+
+            Response.Listener<Bitmap> artworkRequestListener = new Response.Listener<Bitmap>() {
+                @Override
+                public void onResponse(Bitmap artwork) {
+                    ImageView songArtwork = (ImageView) holder.songView.findViewById(R.id.songArtwork);
+                    songArtwork.setImageBitmap(artwork);
+                }
+            };
+
+            ImageRequest artworkRequest = new ImageRequest((String) JsonPath.read(songInfo, "$.artwork"), artworkRequestListener, 0, 0, ImageView.ScaleType.CENTER, Bitmap.Config.ARGB_8888, null);
+            queue.add(artworkRequest);
+            
+            holder.itemView.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View view, MotionEvent event) {
+                    switch(event.getAction()) {
+                        case MotionEvent.ACTION_DOWN:
+                            view.setBackgroundColor(getResources().getColor(R.color.whiteOnHold));
+                            break;
+                        case MotionEvent.ACTION_UP:
+                            view.setBackgroundColor(getResources().getColor(R.color.white));
+                            break;
+                    }
+                    return false;
+                }
+            });
+
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.i("songFragment", "Play song: " + songTitle.getText());
+                }
+            });
+        }
+
+        private String formatTime(Integer second) {
+            Integer minutes = (second % 3600) / 60;
+            Integer seconds = second % 60;
+
+            return String.format("%02d:%02d", minutes, seconds);
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
         }
     }
-
-    Adapter(ArrayList<JSONObject> data) {
-        this.data = data;
-    }
-
-    @Override
-    public Adapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        RelativeLayout v = (RelativeLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.view_song, parent, false);
-        return new ViewHolder(v);
-    }
-
-    @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
-        TextView songTitle = (TextView) holder.songView.findViewById(R.id.songTitle);
-        TextView songArtist = (TextView) holder.songView.findViewById(R.id.songArtist);
-        TextView songDuration = (TextView) holder.songView.findViewById(R.id.songDuration);
-
-        JSONObject songInfoJSON = data.get(position);
-        Object songInfo = Configuration.defaultConfiguration().jsonProvider().parse(songInfoJSON.toString());
-
-        songTitle.setText((String) JsonPath.read(songInfo, "$.title"));
-        songArtist.setText((String) JsonPath.read(songInfo, "$.artist"));
-        songDuration.setText(formatTime((Integer)JsonPath.read(songInfo, "$.duration")));
-
-        Response.Listener<Bitmap> artworkRequestListener = new Response.Listener<Bitmap>() {
-            @Override
-            public void onResponse(Bitmap artwork) {
-                ImageView songArtwork = (ImageView) holder.songView.findViewById(R.id.songArtwork);
-                songArtwork.setImageBitmap(artwork);
-            }
-        };
-
-        ImageRequest artworkRequest = new ImageRequest((String) JsonPath.read(songInfo, "$.artwork"), artworkRequestListener, 0, 0, ImageView.ScaleType.CENTER, Bitmap.Config.ARGB_8888, null);
-
-        queue.add(artworkRequest);
-    }
-
-    private String formatTime(Integer second) {
-        Integer minutes = (second % 3600) / 60;
-        Integer seconds = second % 60;
-
-        return String.format("%02d:%02d", minutes, seconds);
-    }
-
-    @Override
-    public int getItemCount() {
-        return data.size();
-    }
 }
+
