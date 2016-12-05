e4dc8a87af72ae56707c869668bfee77dd17d451
ngockha96 <ngockha96@gmail.com>
2016-12-05 19:44:41 +0700

    Add scan music function to Download Fragment

--- MusicPlayer/app/src/main/java/vn/edu/usth/musicplayer/fragment/DownloadFragment.java
+++ MusicPlayer/app/src/main/java/vn/edu/usth/musicplayer/fragment/DownloadFragment.java
@@ -1,11 +1,18 @@
 package vn.edu.usth.musicplayer.fragment;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import vn.edu.usth.musicplayer.R;
 
 public class DownloadFragment extends Fragment {
@@ -16,6 +23,72 @@ public class DownloadFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_download, container, false);
+        View view = inflater.inflate(R.layout.fragment_download, container, false);
+
+        TextView myTextView = (TextView)view.findViewById(R.id.songItem1);
+
+        getPlayList();
+
+//        String[] stockArr = new String[getPlayList().size()];
+//        stockArr = getPlayList().toArray(stockArr);
+//
+//        int arraySize = getPlayList().size();
+//        for(int i = 0; i < arraySize; i++) {
+//            myTextView.append(stockArr[i]);
+//        }
+        return view;
+    }
+
+    final String MEDIA_PATH = Environment.getExternalStorageDirectory()
+            .getPath() + "/";
+    private ArrayList<HashMap<String, String>> songsList = new ArrayList<HashMap<String, String>>();
+    private String mp3Pattern = ".mp3";
+
+    public ArrayList<HashMap<String, String>> getPlayList() {
+        System.out.println(MEDIA_PATH);
+        if (MEDIA_PATH != null) {
+            File home = new File(MEDIA_PATH);
+            File[] listFiles = home.listFiles();
+            if (listFiles != null && listFiles.length > 0) {
+                for (File file : listFiles) {
+                    System.out.println(file.getAbsolutePath());
+                    if (file.isDirectory()) {
+                        scanDirectory(file);
+                    } else {
+                        addSongToList(file);
+                    }
+                }
+            }
+        }
+        // return songs list array
+        return songsList;
+    }
+
+    private void scanDirectory(File directory) {
+        if (directory != null) {
+            File[] listFiles = directory.listFiles();
+            if (listFiles != null && listFiles.length > 0) {
+                for (File file : listFiles) {
+                    if (file.isDirectory()) {
+                        scanDirectory(file);
+                    } else {
+                        addSongToList(file);
+                    }
+
+                }
+            }
+        }
+    }
+
+    private void addSongToList(File song) {
+        if (song.getName().endsWith(mp3Pattern)) {
+            HashMap<String, String> songMap = new HashMap<String, String>();
+            songMap.put("songTitle",
+                    song.getName().substring(0, (song.getName().length() - 4)));
+            songMap.put("songPath", song.getPath());
+
+            // Adding each song to SongList
+            songsList.add(songMap);
+        }
     }
 }
